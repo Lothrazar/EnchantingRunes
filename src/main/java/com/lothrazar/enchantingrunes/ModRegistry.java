9080991bb3a3c50cfe62530ffe7dd4d2e3031dc4
lothrazar <samson.bassett@gmail.com>
2021-08-03 15:39:42 -0700

    example runes with two nbt recipes

--- src/main/java/com/lothrazar/enchantingrunes/ModRegistry.java
+++ src/main/java/com/lothrazar/enchantingrunes/ModRegistry.java
@@ -2,7 +2,9 @@ package com.lothrazar.enchantingrunes;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,7 +15,8 @@ public class ModRegistry {
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModMain.MODID);
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModMain.MODID);
   public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, ModMain.MODID);
-  //  public static final RegistryObject<Item> OVERWORLD_KEY = ITEMS.register("whatever", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+  public static final RegistryObject<Item> RUNE_A = ITEMS.register("rune_a", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+  public static final RegistryObject<Item> RUNE_B = ITEMS.register("rune_b", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
   //  @SubscribeEvent
   //  public static void onContainerRegistry(final RegistryEvent.Register<ContainerType<?>> event) {
   //    // IForgeRegistry<ContainerType<?>> r = event.getRegistry();
