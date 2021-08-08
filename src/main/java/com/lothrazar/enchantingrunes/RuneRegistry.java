a4845a67c948bb76a5ef18cbb49ad6a486203c17
lothrazar <samson.bassett@gmail.com>
2021-08-07 21:01:54 -0700

    data tags and lore testing

--- src/main/java/com/lothrazar/enchantingrunes/RuneRegistry.java
+++ src/main/java/com/lothrazar/enchantingrunes/RuneRegistry.java
@@ -9,6 +9,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -26,7 +27,7 @@ public class RuneRegistry {
   };
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModMainRunes.MODID);
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModMainRunes.MODID);
-  //  public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, ModMainRunes.MODID);
+  public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, ModMainRunes.MODID);
   public static final RegistryObject<Item> BALANCE_RUNE = ITEMS.register("balance_rune", () -> new Item(new Item.Properties().group(TAB)));
   public static final RegistryObject<Item> BLOOD_RUNE = ITEMS.register("blood_rune", () -> new Item(new Item.Properties().group(TAB)));
   public static final RegistryObject<Item> CHAOS_RUNE = ITEMS.register("chaos_rune", () -> new Item(new Item.Properties().group(TAB)));
@@ -42,6 +43,7 @@ public class RuneRegistry {
   public static final RegistryObject<Item> RUNE_U = ITEMS.register("rune_u", () -> new Item(new Item.Properties().group(TAB)));
   public static final RegistryObject<Item> RUNE_Y = ITEMS.register("rune_y", () -> new Item(new Item.Properties().group(TAB)));
   public static final RegistryObject<Item> RUNE_ITH = ITEMS.register("rune_ith", () -> new Item(new Item.Properties().group(TAB)));
+  //stone
   public static final RegistryObject<Item> BLADE_MASON = ITEMS.register("masonry_blade", () -> new KnifeItem(new Item.Properties().group(TAB)));
   public static final RegistryObject<Block> STONE_LAYERS = BLOCKS.register("stone_layer", () -> new BlockLayering(AbstractBlock.Properties.create(Material.ROCK)));
   public static final RegistryObject<Item> STONE_LAYERS_I = ITEMS.register("stone_layer", () -> new BlockItem(STONE_LAYERS.get(), new Item.Properties().group(TAB)));
