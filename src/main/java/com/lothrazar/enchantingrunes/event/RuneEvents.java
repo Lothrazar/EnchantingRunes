f5b6187ee298464357aac2869470aeb82e1926f5
lothrazar <samson.bassett@gmail.com>
2021-08-08 11:36:09 -0700

    Lore runewords: some random and some fixed

--- src/main/java/com/lothrazar/enchantingrunes/event/RuneEvents.java
+++ src/main/java/com/lothrazar/enchantingrunes/event/RuneEvents.java
@@ -1,16 +1,122 @@
 package com.lothrazar.enchantingrunes.event;
 
 import com.lothrazar.enchantingrunes.ModMainRunes;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.TableLootEntry;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.StringNBT;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.LootTableLoadEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent.ItemCraftedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class RuneEvents {
 
+  public static final ITag.INamedTag<Item> RUNESTONE = ItemTags.createOptional(new ResourceLocation(ModMainRunes.MODID, "runes/stone"));
+
   @SubscribeEvent
-  public static void onLootLoad(LootTableLoadEvent event) {
+  public void test(ItemCraftedEvent event) {
+    ItemStack crafting = event.getCrafting();
+    if (crafting.getTag() != null && crafting.getTag().contains(ModMainRunes.MODID)) {
+      if (crafting.getTag().contains("level")) {
+        //maybe
+        Map<Enchantment, Integer> oldEnch = EnchantmentHelper.getEnchantments(crafting);
+        EnchantmentHelper.setEnchantments(new HashMap<Enchantment, Integer>(), crafting);
+        applyRandomEnch(event, crafting);
+        if (oldEnch != null && !oldEnch.isEmpty()) {
+          this.merge(oldEnch, crafting);
+        }
+      }
+      CompoundNBT modTag = crafting.getChildTag(ModMainRunes.MODID);
+      if (modTag.contains("lore") && modTag.getBoolean("lore")) {
+        //        tryRuneword(event);
+        this.generateLore(event, crafting);
+      }
+      //onlyh remove after done with it
+      crafting.getTag().remove(ModMainRunes.MODID);
+      System.out.println("FINAL " + crafting.getTag());
+    }
+  }
+
+  private void generateLore(ItemCraftedEvent event, ItemStack crafting) {
+    if (event.getInventory() instanceof CraftingInventory) {
+      String lore = "";
+      CraftingInventory test = (CraftingInventory) event.getInventory();
+      for (int i = 0; i < test.getSizeInventory(); i++) {
+        ItemStack stak = test.getStackInSlot(i);
+        if (stak.getItem().isIn(RUNESTONE)) {
+          lore += stak.getDisplayName().getString();
+        }
+      }
+      //      System.out.println("GENLORE " + lore);
+      CompoundNBT displayTag = new CompoundNBT();
+      //      displayTag.pu
+      ListNBT tagList = new ListNBT();
+      String escaped = "{\"text\":\"" + lore + "\",\"color\":\"gold\"}";
+      tagList.add(StringNBT.valueOf(escaped));
+      displayTag.put("Lore", tagList);
+      displayTag.putString("Name", "TEST");
+      crafting.getTag().put("display", displayTag);
+      System.out.println(" crafting.getTag() " + crafting.getTag());
+    }
+    //    "display": {
+    //      "Lore": [
+    //        "[{\"translate\":\"item.enchantingrunes.rune_a\",\"color\":\"gold\"},{\"translate\":\"item.enchantingrunes.rune_c\",\"color\":\"gold\"}]"
+    //      ]
+    //    },
+  }
+  //  private void tryRuneword(ItemCraftedEvent event) {
+  //    if (event.getInventory() instanceof CraftingInventory) {
+  //      CraftingInventory test = (CraftingInventory) event.getInventory();
+  //      for (int i = 0; i < test.getSizeInventory(); i++) {
+  //        ItemStack stak = test.getStackInSlot(0);
+  //        if (stak.getItem().isIn(RUNESTONE)) {
+  //          System.out.println(i + " wsas input " + stak);
+  //        }
+  //      }
+  //    }
+  //  }
+
+  private void applyRandomEnch(ItemCraftedEvent event, ItemStack crafting) {
+    CompoundNBT tag = crafting.getChildTag(ModMainRunes.MODID);
+    EnchantmentHelper.addRandomEnchantment(event.getPlayer().world.rand, crafting, tag.getInt("level"), tag.getBoolean("treasure"));
+  }
+
+  private void merge(Map<Enchantment, Integer> oldEnch, ItemStack crafting) {
+    Map<Enchantment, Integer> newEnch = EnchantmentHelper.getEnchantments(crafting);
+    //anything in new thats also in old, merge it over
+    for (Entry<Enchantment, Integer> newEntry : newEnch.entrySet()) {
+      //
+      //if this exists in the old list, merge into new
+      if (oldEnch.containsKey(newEntry.getKey())) {
+        //take max of each
+        newEnch.put(newEntry.getKey(), Math.max(newEntry.getValue(), oldEnch.get(newEntry.getKey())));
+      }
+    }
+    //anything in old thats NOT in new
+    for (Entry<Enchantment, Integer> oldEntry : oldEnch.entrySet()) {
+      if (!newEnch.containsKey(oldEntry.getKey())) {
+        //new list does NOT hvae this thing from old
+        newEnch.put(oldEntry.getKey(), oldEntry.getValue());
+      }
+    }
+    EnchantmentHelper.setEnchantments(newEnch, crafting);
+  }
+
+  //  pub
+  @SubscribeEvent
+  public void onLootLoad(LootTableLoadEvent event) {
     if (event.getName().equals(new ResourceLocation("minecraft", "granite"))) {
       event.getTable().addPool(LootPool.builder().addEntry(TableLootEntry.builder(new ResourceLocation(ModMainRunes.MODID, "granite"))).build());
     }
--- src/main/java/com/lothrazar/enchantingrunes/item/KnifeItem.java
+++ src/main/java/com/lothrazar/enchantingrunes/item/KnifeItem.java
@@ -2,6 +2,8 @@ package com.lothrazar.enchantingrunes.item;
 
 import com.lothrazar.enchantingrunes.RuneRegistry;
 import com.lothrazar.enchantingrunes.block.BlockLayering;
+import java.util.Arrays;
+import java.util.List;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.item.ItemEntity;
@@ -19,7 +21,6 @@ import net.minecraft.world.biome.Biome;
 
 public class KnifeItem extends Item {
 
-  private static final double CHANCE_MAKE_RUNESTONE = 0.90; // when using blade
   private static final int COOLDOWN = 8;
 
   public KnifeItem(Properties properties) {
@@ -79,54 +80,95 @@ public class KnifeItem extends Item {
   private void dropItem(ItemUseContext context) { //flip a coin; if pass then drop one
     World world = context.getWorld();
     BlockPos pos = context.getPos();
-    Biome b = world.getBiome(pos);
-    System.out.println("b " + b);
-    System.out.println("cat " + b.getCategory());
-    ItemStack rune = new ItemStack(RuneRegistry.STONE_LAYERS_I.get());
-    if (world.rand.nextDouble() < CHANCE_MAKE_RUNESTONE) {
-      // 10 % chance it stays as stone layer item
-      Item[] pick = new Item[] { RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_ITH.get() };
-      switch (b.getCategory()) {
-        case BEACH:
+    final double dice = world.rand.nextDouble();
+    List<Item> pick = Arrays.asList(new Item[] {
+        RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_A.get(),
+        RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_E.get(),
+        RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_I.get(),
+        RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_O.get(),
+        RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_U.get(),
+        RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_Y.get(),
+        RuneRegistry.RUNE_ITH.get()
+    });
+    if (dice < 0.01) {
+      //first X% is stone layer
+      dropHere(world, pos, new ItemStack(RuneRegistry.STONE_LAYERS_I.get()));
+    }
+    else {
+      //first, you have a low chance to get ANY rune, weighted random defined in above array contents
+      //doubly weight it based on biome bonus
+      Biome biome = world.getBiome(pos);
+      switch (biome.getCategory()) {
         case JUNGLE:
-          pick = new Item[] { RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get() };
-        break;
-        case EXTREME_HILLS:
-        case FOREST:
-          pick = new Item[] { RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_U.get() };
+          //add weight
+          pick.add(RuneRegistry.RUNE_ITH.get());
+          pick.add(RuneRegistry.RUNE_ITH.get());
+          pick.add(RuneRegistry.RUNE_U.get());
+          pick.add(RuneRegistry.RUNE_U.get());
         break;
         case ICY:
-          pick = new Item[] { RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_U.get() };
+          pick.add(RuneRegistry.RUNE_I.get());
+          pick.add(RuneRegistry.RUNE_I.get());
+          pick.add(RuneRegistry.RUNE_I.get());
+          pick.add(RuneRegistry.RUNE_I.get());
         break;
         case DESERT:
         case MESA:
         case SAVANNA:
-          pick = new Item[] { RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_Y.get() };
+          pick.add(RuneRegistry.RUNE_O.get());
+          pick.add(RuneRegistry.RUNE_O.get());
+          pick.add(RuneRegistry.RUNE_O.get());
+          pick.add(RuneRegistry.RUNE_O.get());
         break;
         case MUSHROOM:
-          pick = new Item[] { RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_ITH.get() };
+          pick.add(RuneRegistry.RUNE_U.get());
+          pick.add(RuneRegistry.RUNE_Y.get());
+          pick.add(RuneRegistry.RUNE_U.get());
+          pick.add(RuneRegistry.RUNE_Y.get());
+          pick.add(RuneRegistry.RUNE_U.get());
+          pick.add(RuneRegistry.RUNE_Y.get());
         break;
         case NETHER:
-        case NONE:
-          pick = new Item[] { RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_ITH.get() };
-        break;
-        case PLAINS:
-          pick = new Item[] { RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_ITH.get() };
-        break;
-        case OCEAN:
-        case RIVER:
-          pick = new Item[] { RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get() };
+          pick.add(RuneRegistry.RUNE_ITH.get());
+          pick.add(RuneRegistry.RUNE_U.get());
+          pick.add(RuneRegistry.RUNE_U.get());
         break;
         case SWAMP:
         case TAIGA:
-          pick = new Item[] { RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_Y.get() };
+          pick.add(RuneRegistry.RUNE_E.get());
+          pick.add(RuneRegistry.RUNE_E.get());
+          pick.add(RuneRegistry.RUNE_E.get());
+          pick.add(RuneRegistry.RUNE_E.get());
         break;
         case THEEND:
-          pick = new Item[] { RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_ITH.get(), RuneRegistry.RUNE_ITH.get(), RuneRegistry.RUNE_ITH.get() };
+          pick.add(RuneRegistry.RUNE_ITH.get());
+          pick.add(RuneRegistry.RUNE_ITH.get());
+          pick.add(RuneRegistry.RUNE_ITH.get());
+          pick.add(RuneRegistry.RUNE_ITH.get());
+        break;
+        case BEACH:
+        break;
+        case EXTREME_HILLS:
+        break;
+        case FOREST:
+        break;
+        case NONE:
+        break;
+        case OCEAN:
+        break;
+        case PLAINS:
+        break;
+        case RIVER:
+        break;
+        default:
         break;
       }
-      rune = new ItemStack(pick[world.rand.nextInt(pick.length)]);
+      //final drop. exactly always one thing
+      dropHere(world, pos, new ItemStack(pick.get(world.rand.nextInt(pick.size()))));
     }
+  }
+
+  private void dropHere(World world, BlockPos pos, ItemStack rune) {
     if (!world.isRemote) {
       world.addEntity(new ItemEntity(world, pos.getX() + 0.5F, pos.getY(), pos.getZ() + 0.5F, rune));
     }
