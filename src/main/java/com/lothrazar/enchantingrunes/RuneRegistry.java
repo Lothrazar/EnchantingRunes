1201c3638a6f48efadf8b7b21aed70e11b6f8089
lothrazar <samson.bassett@gmail.com>
2021-08-07 17:26:20 -0700

    new names and assets

--- src/main/java/com/lothrazar/enchantingrunes/RuneRegistry.java
+++ src/main/java/com/lothrazar/enchantingrunes/RuneRegistry.java
@@ -21,29 +21,28 @@ public class RuneRegistry {
 
     @Override
     public ItemStack createIcon() {
-      return new ItemStack(BLANK_RUNE.get());
+      return new ItemStack(HUMILITY_RUNE.get());
     }
   };
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModMainRunes.MODID);
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModMainRunes.MODID);
   //  public static final DeferredRegister<TileEntityType<?>> TILE_ENTITIES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, ModMainRunes.MODID);
-  public static final RegistryObject<Item> BLANK_RUNE = ITEMS.register("blank_rune", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> BALANCE_RUNE = ITEMS.register("balance_rune", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> BLOOD_RUNE = ITEMS.register("blood_rune", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> CHAOS_RUNE = ITEMS.register("chaos_rune", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> COSMIC_RUNE = ITEMS.register("cosmic_rune", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> HUMILITY_RUNE = ITEMS.register("humility_rune", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> RADIANCE_RUNE = ITEMS.register("radiance_rune", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> VOID_RUNE = ITEMS.register("void_rune", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> WISDOM_RUNE = ITEMS.register("wisdom_rune", () -> new Item(new Item.Properties().group(TAB)));
   public static final RegistryObject<Item> RUNE_A = ITEMS.register("rune_a", () -> new Item(new Item.Properties().group(TAB)));
-  public static final RegistryObject<Item> RUNE_B = ITEMS.register("rune_b", () -> new Item(new Item.Properties().group(TAB)));
-  public static final RegistryObject<Item> RUNE_C = ITEMS.register("rune_c", () -> new Item(new Item.Properties().group(TAB)));
-  public static final RegistryObject<Item> RUNE_D = ITEMS.register("rune_d", () -> new Item(new Item.Properties().group(TAB)));
   public static final RegistryObject<Item> RUNE_E = ITEMS.register("rune_e", () -> new Item(new Item.Properties().group(TAB)));
-  public static final RegistryObject<Item> RUNE_F = ITEMS.register("rune_f", () -> new Item(new Item.Properties().group(TAB)));
-  public static final RegistryObject<Item> RUNE_G = ITEMS.register("rune_g", () -> new Item(new Item.Properties().group(TAB)));
-  public static final RegistryObject<Item> BLADE_MASON = ITEMS.register("blade_mason", () -> new KnifeItem(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> RUNE_I = ITEMS.register("rune_i", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> RUNE_O = ITEMS.register("rune_o", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> RUNE_U = ITEMS.register("rune_u", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> RUNE_Y = ITEMS.register("rune_y", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> RUNE_ITH = ITEMS.register("rune_ith", () -> new Item(new Item.Properties().group(TAB)));
+  public static final RegistryObject<Item> BLADE_MASON = ITEMS.register("masonry_blade", () -> new KnifeItem(new Item.Properties().group(TAB)));
   public static final RegistryObject<Block> STONE_LAYERS = BLOCKS.register("stone_layer", () -> new BlockLayering(AbstractBlock.Properties.create(Material.ROCK)));
   public static final RegistryObject<Item> STONE_LAYERS_I = ITEMS.register("stone_layer", () -> new BlockItem(STONE_LAYERS.get(), new Item.Properties().group(TAB)));
-  /**
-   * 
-   * fire water earth air = 4 void, life, time = 3 ? time/temporal/ghost cosmic. insight. truth. love. willpower. fear. blood. survival, creativity.
-   */
-  //  @SubscribeEvent
-  //  public static void onContainerRegistry(final RegistryEvent.Register<ContainerType<?>> event) {
-  //    // IForgeRegistry<ContainerType<?>> r = event.getRegistry();
-  //  }
 }
--- src/main/java/com/lothrazar/enchantingrunes/item/KnifeItem.java
+++ src/main/java/com/lothrazar/enchantingrunes/item/KnifeItem.java
@@ -5,6 +5,7 @@ import com.lothrazar.enchantingrunes.block.BlockLayering;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
@@ -14,40 +15,120 @@ import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 
 public class KnifeItem extends Item {
 
-  private static final int COOLDOWN = 10;
+  private static final double CHANCE_MAKE_RUNESTONE = 0.90; // when using blade
+  private static final int COOLDOWN = 8;
 
   public KnifeItem(Properties properties) {
-    super(properties);
+    super(properties.maxDamage(64 * 2));
   }
 
   @Override
   public ActionResultType onItemUse(ItemUseContext context) {
-    if (context.getPlayer().getCooldownTracker().hasCooldown(this)) {
+    PlayerEntity player = context.getPlayer();
+    if (player.getCooldownTracker().hasCooldown(this)) {
+      return ActionResultType.PASS;
+    }
+    if (context.getFace() == Direction.DOWN) {
+      //sides and top ok
       return ActionResultType.PASS;
     }
     World world = context.getWorld();
     BlockPos pos = context.getPos();
     BlockState state = world.getBlockState(pos);
     ItemStack held = context.getItem();
-    boolean valid = state.getBlock() == Blocks.STONE || state.getBlock() == RuneRegistry.STONE_LAYERS.get();
-    if (context.getFace() == Direction.UP && valid) {
-      context.getPlayer().getCooldownTracker().setCooldown(this, COOLDOWN);
-      ItemStack rune = new ItemStack(RuneRegistry.BLANK_RUNE.get());
-      world.addEntity(new ItemEntity(world, pos.getX() + 0.5F, pos.getY(), pos.getZ() + 0.5F, rune));
-      int oldLayer = state.hasProperty(BlockLayering.LAYERS) ? state.get(BlockLayering.LAYERS) : 8;
-      int newLayer = oldLayer - 1;
-      if (newLayer > 0) {
-        world.setBlockState(pos, RuneRegistry.STONE_LAYERS.get().getDefaultState().with(BlockLayering.LAYERS, newLayer));
-      }
-      else {
-        world.setBlockState(pos, Blocks.AIR.getDefaultState());
-      }
-      world.playSound(context.getPlayer(), pos, SoundEvents.UI_STONECUTTER_TAKE_RESULT, SoundCategory.BLOCKS, 1, 1);
-      held.damageItem(1, context.getPlayer(), p -> p.setHeldItem(context.getHand(), ItemStack.EMPTY));
+    boolean valid = this.isValid(context);
+    if (valid) {
+      player.getCooldownTracker().setCooldown(this, COOLDOWN);
+      player.swingArm(context.getHand());
+      world.playSound(player, pos, SoundEvents.UI_STONECUTTER_TAKE_RESULT, SoundCategory.BLOCKS, 1, 1);
+      held.damageItem(1, player, p -> p.setHeldItem(context.getHand(), ItemStack.EMPTY));
+      dropItem(context);
+      reduceLayer(world, pos, state);
+      return ActionResultType.SUCCESS;
     }
     return ActionResultType.PASS;
+  }
+
+  private boolean isValid(ItemUseContext context) {
+    World world = context.getWorld();
+    BlockPos pos = context.getPos();
+    BlockState state = world.getBlockState(pos);
+    boolean validStone = state.getBlock() == Blocks.STONE_SLAB || state.getBlock() == Blocks.STONE || state.getBlock() == RuneRegistry.STONE_LAYERS.get();
+    return validStone;
+  }
+
+  private void reduceLayer(World world, BlockPos pos, BlockState state) {
+    final int fullLay = 8;
+    int oldLayer = state.hasProperty(BlockLayering.LAYERS) ? state.get(BlockLayering.LAYERS) : fullLay;
+    if (state.getBlock() == Blocks.STONE_SLAB) {
+      oldLayer = fullLay / 2;
+    }
+    int newLayer = oldLayer - 1;
+    if (newLayer > 0) {
+      world.setBlockState(pos, RuneRegistry.STONE_LAYERS.get().getDefaultState().with(BlockLayering.LAYERS, newLayer));
+    }
+    else {
+      world.setBlockState(pos, Blocks.AIR.getDefaultState());
+    }
+  }
+
+  private void dropItem(ItemUseContext context) { //flip a coin; if pass then drop one
+    World world = context.getWorld();
+    BlockPos pos = context.getPos();
+    Biome b = world.getBiome(pos);
+    System.out.println("b " + b);
+    System.out.println("cat " + b.getCategory());
+    ItemStack rune = new ItemStack(RuneRegistry.STONE_LAYERS_I.get());
+    if (world.rand.nextDouble() < CHANCE_MAKE_RUNESTONE) {
+      // 10 % chance it stays as stone layer item
+      Item[] pick = new Item[] { RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_ITH.get() };
+      switch (b.getCategory()) {
+        case BEACH:
+        case JUNGLE:
+          pick = new Item[] { RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get() };
+        break;
+        case EXTREME_HILLS:
+        case FOREST:
+          pick = new Item[] { RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_U.get() };
+        break;
+        case ICY:
+          pick = new Item[] { RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_U.get() };
+        break;
+        case DESERT:
+        case MESA:
+        case SAVANNA:
+          pick = new Item[] { RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_Y.get() };
+        break;
+        case MUSHROOM:
+          pick = new Item[] { RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_ITH.get() };
+        break;
+        case NETHER:
+        case NONE:
+          pick = new Item[] { RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_A.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_ITH.get() };
+        break;
+        case PLAINS:
+          pick = new Item[] { RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_ITH.get() };
+        break;
+        case OCEAN:
+        case RIVER:
+          pick = new Item[] { RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_I.get(), RuneRegistry.RUNE_O.get() };
+        break;
+        case SWAMP:
+        case TAIGA:
+          pick = new Item[] { RuneRegistry.RUNE_E.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_U.get(), RuneRegistry.RUNE_Y.get() };
+        break;
+        case THEEND:
+          pick = new Item[] { RuneRegistry.RUNE_O.get(), RuneRegistry.RUNE_Y.get(), RuneRegistry.RUNE_ITH.get(), RuneRegistry.RUNE_ITH.get(), RuneRegistry.RUNE_ITH.get() };
+        break;
+      }
+      rune = new ItemStack(pick[world.rand.nextInt(pick.length)]);
+    }
+    if (!world.isRemote) {
+      world.addEntity(new ItemEntity(world, pos.getX() + 0.5F, pos.getY(), pos.getZ() + 0.5F, rune));
+    }
   }
 }
