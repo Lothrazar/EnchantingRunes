fa633b1f413fc7a57d784da4e6117bc704ec9d68
Sam <lothrazar@hotmail.com>
2026-08-25 15:20:38 -0700

    porting 26.1

--- src/main/java/com/lothrazar/enchantingrunes/RegistryRunes.java
+++ src/main/java/com/lothrazar/enchantingrunes/RegistryRunes.java
@@ -20,22 +20,22 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 // @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RegistryRunes {
 
-  public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, ModMainRunes.MODID);
-  public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Registries.BLOCK, ModMainRunes.MODID);
+  public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ModMainRunes.MODID);
+  public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(ModMainRunes.MODID);
   public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ModMainRunes.MODID);
 
-  public static final DeferredHolder<Item, RuneItem> RUNE_A = ITEMS.register("rune_a", () -> new RuneItem(new Item.Properties().rarity(Rarity.EPIC)));
-  public static final DeferredHolder<Item, RuneItem> RUNE_E = ITEMS.register("rune_e", () -> new RuneItem(new Item.Properties().rarity(Rarity.EPIC)));
-  public static final DeferredHolder<Item, RuneItem> RUNE_I = ITEMS.register("rune_i", () -> new RuneItem(new Item.Properties().rarity(Rarity.EPIC)));
-  public static final DeferredHolder<Item, RuneItem> RUNE_O = ITEMS.register("rune_o", () -> new RuneItem(new Item.Properties().rarity(Rarity.EPIC)));
-  public static final DeferredHolder<Item, RuneItem> RUNE_U = ITEMS.register("rune_u", () -> new RuneItem(new Item.Properties().rarity(Rarity.EPIC)));
-  public static final DeferredHolder<Item, RuneItem> RUNE_Y = ITEMS.register("rune_y", () -> new RuneItem(new Item.Properties().rarity(Rarity.EPIC)));
-  public static final DeferredHolder<Item, RuneItem> RUNE_TH = ITEMS.register("rune_th", () -> new RuneItem(new Item.Properties().rarity(Rarity.EPIC)));
-  public static final DeferredHolder<Item, RuneItem> RUNE_BLANK = ITEMS.register("rune_blank", () -> new RuneItem(new Item.Properties()));
+  public static final DeferredHolder<Item, RuneItem> RUNE_A = ITEMS.registerItem("rune_a", props -> new RuneItem(props.rarity(Rarity.EPIC)));
+  public static final DeferredHolder<Item, RuneItem> RUNE_E = ITEMS.registerItem("rune_e", props -> new RuneItem(props.rarity(Rarity.EPIC)));
+  public static final DeferredHolder<Item, RuneItem> RUNE_I = ITEMS.registerItem("rune_i", props -> new RuneItem(props.rarity(Rarity.EPIC)));
+  public static final DeferredHolder<Item, RuneItem> RUNE_O = ITEMS.registerItem("rune_o", props -> new RuneItem(props.rarity(Rarity.EPIC)));
+  public static final DeferredHolder<Item, RuneItem> RUNE_U = ITEMS.registerItem("rune_u", props -> new RuneItem(props.rarity(Rarity.EPIC)));
+  public static final DeferredHolder<Item, RuneItem> RUNE_Y = ITEMS.registerItem("rune_y", props -> new RuneItem(props.rarity(Rarity.EPIC)));
+  public static final DeferredHolder<Item, RuneItem> RUNE_TH = ITEMS.registerItem("rune_th", props -> new RuneItem(props.rarity(Rarity.EPIC)));
+  public static final DeferredHolder<Item, RuneItem> RUNE_BLANK = ITEMS.registerItem("rune_blank", props -> new RuneItem(props));
   //stone
-  public static final DeferredHolder<Item, Item> BLADE_MASON = ITEMS.register("masonry_blade", () -> new KnifeItem(new Item.Properties()));
-  public static final DeferredHolder<Block, BlockLayering> STONE_LAYERS = BLOCKS.register("stone_layer", () -> new BlockLayering(Blocks.STONE, BlockBehaviour.Properties.of()));
-  public static final DeferredHolder<Item, Item> STONE_LAYERS_I = ITEMS.register("stone_layer", () -> new BlockItemFlib(STONE_LAYERS.get(), new Item.Properties().useBlockDescriptionPrefix()));
+  public static final DeferredHolder<Item, Item> BLADE_MASON = ITEMS.registerItem("masonry_blade", props -> new KnifeItem(props));
+  public static final DeferredHolder<Block, BlockLayering> STONE_LAYERS = BLOCKS.registerBlock("stone_layer", props -> new BlockLayering(Blocks.STONE, props));
+  public static final DeferredHolder<Item, Item> STONE_LAYERS_I = ITEMS.registerItem("stone_layer", props -> new BlockItemFlib(STONE_LAYERS.get(), props.useBlockDescriptionPrefix()));
 
   public static final DeferredHolder<CreativeModeTab, CreativeModeTab> TAB = TABS.register("tab", () -> CreativeModeTab.builder()
       .icon(() -> new ItemStack(RUNE_TH.get()))
